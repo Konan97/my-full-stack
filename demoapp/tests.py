3ee512082454bbbabde5235ff41d32061e686e53
Sun <YSUN98@volvocars.com>
2024-07-09 09:32:11 -0400

    add test case

--- demoapp/tests.py
+++ demoapp/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from .models import Car
 # Create your tests here.
+
+class CarModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.Car = Car.objects.create(
+            name = 'EX90',
+            Rfid = 000000,
+            ATACQ_Item = ""
+        )
+    
+    def test_field(self):
+        self.assertIsInstance(self.Car.Rfid, int)
+
+        self.assertIsInstance(self.Car.name, int)
